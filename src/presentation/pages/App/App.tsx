6e93bb0b74c06002b6061e571065bd8bf6e24f5b
unknown <evgeniy.popov.jsx@gmail.com>
2024-08-20 12:51:07 +0300

    created a structure

---
+++ src/presentation/pages/App/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+import './App.css'
+import { RouterConfig } from '../../../application/routes/routerConfig';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RouterConfig />
+    </BrowserRouter>
+  );
+}
+
+export default App
