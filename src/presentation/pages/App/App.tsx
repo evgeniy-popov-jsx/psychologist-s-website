17dd7b28887e0179f40c7947f165f4e776406772
unknown <evgeniy.popov.jsx@gmail.com>
2024-09-02 16:05:46 +0300

    fixed scroll route

--- src/presentation/pages/App/App.tsx
+++ src/presentation/pages/App/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { RouterConfig } from 'application/routes/routerConfig';
 import { Menu } from 'presentation/components/Menu';
+import ScrollToTop from 'presentation/components/ScrollToTop';
 
 function App() {
+
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <RouterConfig />
       <Menu />
     </BrowserRouter>
